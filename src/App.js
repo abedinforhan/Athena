0483eefdde0e183e51447edd90b2cf18c5b3078b
osthirfarhan <farhanengn@gmail.com>
2020-11-19 02:07:24 +0600

    home setup done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-
 import './App.css';
+import Home from './Componemts/Home/Home';
 
 function App() {
   return (
     <div className="app">
-       <h2>Our Main Component</h2>
-       <h1 className="text-danger">Marjia Zaman</h1>
-        <h2 className="text-warning">Mezbaul abedin Forhan</h2>
+      <Home/>
    </div>
   );
 }
