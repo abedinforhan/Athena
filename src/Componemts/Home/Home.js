8c9aacf8cf997382f8cdb04c47ac30dad1c7feb7
osthirfarhan <farhanengn@gmail.com>
2020-11-19 22:58:42 +0600

    footer and sevice price completed 80%"

--- src/Componemts/Home/Home.js
+++ src/Componemts/Home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Footer from '../Footer/Footer';
+import ServicePrice from '../ServicePrice/ServicePrice';
 import './Home.css';
+
 
 const Home = () => {
     return (
         <div className="home">
-            This is home
+        <ServicePrice/>
+         <Footer/>
         </div>
     );
 };
