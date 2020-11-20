9fb906f8df2151e96fbaa75aa438d32e1483dd23
marjia-anee <marjiazaman@gmail.com>
2020-11-20 14:22:20 +0600

    merging

--- src/Components/Home/WorkDetails/WorkDetails.js
+++ src/Components/Home/WorkDetails/WorkDetails.js
@@ -1,41 +1,49 @@
 import React from 'react';
-import { default as img1, default as img4 } from '../../../Images/img1.png';
+import img1 from '../../../Images/img1.png';
 import img2 from '../../../Images/img2.png';
 import img3 from '../../../Images/img3.png';
+import img4 from '../../../Images/img4.png';
 import './WorkDetails.css';
 
 const WorkDetails = () => {
       return (
-            <section className = "workDetails container ">
+            <section className = "container">
                   <div>
-                        <h1 className="section-name text-center">What we do</h1>
+                        <h2 className="section-name text-center">What we do</h2>
                         <p className="text-secondary text-center">Our main focus is to make the User Experience very <br/> simple and easy. Simplicity is our Strength.</p>
                   </div>
-                 
-                  <div className=" info-cards">
+                  <div className="row info-cards">
                         <div className="row d-flex justify-content-center align-items-center">
-                              <div className="info-card-item col-12 col-sm-6 col-md-3 col-lg-3 text-center">
+                              <div className="col-12 col-sm-6 col-md-3 col-lg-3 text-center info-card-item">
                                     <img src={img1} alt="" className="img-fluid"/>
-                                    <h2 className="">Experience Design</h2>
-                                    <p className = "text-muted">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut, necessitatibus?</p>
+                                    <br/>
+                                    <h3 className="">Experience Design</h3>
+                                    <p className = "text-muted">The point of using Lorem Ipsum is that it has a
+                            more-or less normal.</p>
                               </div>
 
-                              <div className="info-card-item col-12 col-sm-6 col-md-3 col-lg-3 text-center">
+                              <div className="col-12 col-sm-6 col-md-3 col-lg-3 text-center info-card-item">
                                     <img src={img2} alt="" className="img-fluid"/>
-                                    <h2 className="">Experience Design</h2>
-                                    <p className = "text-muted">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut, necessitatibus?</p>
+                                    <br/>
+                                    <h3 className="">Interface Design</h3>
+                                    <p className = "text-muted">The point of using Lorem Ipsum is that it has a
+                            more-or less normal.</p>
                               </div>
 
-                              <div className="info-card-item col-12 col-sm-6 col-md-3 col-lg-3 text-center">
+                              <div className="col-12 col-sm-6 col-md-3 col-lg-3 text-center info-card-item">
                                     <img src={img3} alt="" className="img-fluid"/>
-                                    <h2 className="">Experience Design</h2>
-                                    <p className = "text-muted">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut, necessitatibus?</p>
+                                    <br/>
+                                    <h3 className="">Prototyping</h3>
+                                    <p className = "text-muted">The point of using Lorem Ipsum is that it has a
+                            more-or less normal.</p>
                               </div>
 
-                              <div className="info-card-item col-12 col-sm-6 col-md-3 col-lg-3 text-center">
+                              <div className="col-12 col-sm-6 col-md-3 col-lg-3 text-center info-card-item">
                                     <img src={img4} alt="" className="img-fluid"/>
-                                    <h2 className="">Experience Design</h2>
-                                    <p className = "text-muted">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut, necessitatibus?</p>
+                                    <br/>
+                                    <h3 className="">Illustration</h3>
+                                    <p className = "text-muted">The point of using Lorem Ipsum is that it has a
+                            more-or less normal.</p>
                               </div>
 
                         </div>
